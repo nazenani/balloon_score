f8d14b4fd3027482901647ddcaa5828425421e71
nishida <nishida@wilm.jp>
2016-03-11 19:09:40 +0900

    タイトルページのカスタマイズ

--- js/main.js
+++ js/main.js
@@ -22,6 +22,105 @@ var GRAVITY       = 0.5; // 重力
 var SCREEN_WIDTH  = 640; // 横幅
 var SCREEN_HEIGHT = 960; // 縦幅
 
+// TitleScene クラスを定義
+phina.define('TitleScene', {
+	superClass: 'DisplayScene',
+	// コンストラクタ
+	init: function() {
+		this.superInit();
+		// 背景
+		this.backgroundColor = 'skyblue';
+
+		// 縦線
+		var lineY = RectangleShape({
+			width: 2,
+			height: this.gridY.span(16),
+			fill: 'black',
+		}).addChildTo(this).setPosition(this.gridX.center(), this.gridY.center());
+		lineY.alpha = 0.5;
+
+		// タイトル
+		Label({
+			text:'タッチしてバルーンGET',
+			fontSize: 62,
+			fill: 'white',
+		}).addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(4));
+
+		// 説明左
+		Label({
+			text:'画面の左側を\nタッチすると\n左上方向へ',
+			fontSize: 32,
+			fill: 'white',
+		}).addChildTo(this).setPosition(this.gridX.center(-4), this.gridY.center());
+		// 説明右
+		Label({
+			text:'画面の右側を\nタッチすると\n右上方向へ',
+			fontSize: 32,
+			fill: 'white',
+		}).addChildTo(this).setPosition(this.gridX.center(4), this.gridY.center());
+		// 説明中心
+		Label({
+			text:'それぞれジャンプします',
+			fontSize: 32,
+			fill: 'white',
+		}).addChildTo(this).setPosition(this.gridX.center(), this.gridY.center(2));
+
+		// 開始ラベル
+		var start = Start().addChildTo(this);
+		start.x = this.gridX.center()
+		start.bottom = this.gridY.span(13);
+		start.changeSize();
+
+		// 床
+		this.floor = RectangleShape({
+			width: this.gridX.width,
+			height: this.gridY.span(1),
+			fill: 'silver',
+		}).addChildTo(this).setPosition(this.gridX.center(), this.gridY.center(4));
+		this.floor.alpha = 0.0;
+
+		// プレイヤー
+		this.player = Player('tomapiko').addChildTo(this).setPosition(this.gridX.center(), this.floor.top);
+		// 初速度を与える
+		this.player.physical.force(-3, 0);
+		// 初期アニメーション
+		this.player.anim.gotoAndPlay('left');
+
+		// 画面上でタッチが離れた時
+		this.onpointend = function(e) {
+			// 次のシーン
+			this.exit('main');
+		}
+	},
+
+	update: function(app) {
+		// 床とヒットしたら
+		if (this.player.hitTestElement(this.floor)) {
+			// y方向の速度と重力を無効にする
+			this.player.physical.velocity.y = 0;
+			this.player.physical.gravity.y = 0;
+			// 位置調整
+			this.player.bottom = this.floor.top;
+			// フラグ立て
+			this.player.isOnFloor = true;
+			// アニメーション変更
+			this.player.anim.gotoAndPlay('left');
+		}
+
+		// 100フレーム毎にジャンプ
+		if ((app.ticker.frame % 100) == 0) {
+			// 上方向に速度を与える（ジャンプ）
+			this.player.physical.velocity.y = -JUMP_POWER;
+			// 重力復活
+			this.player.physical.gravity.y = GRAVITY;
+			// フラグ変更
+			this.player.isOnFloor = false;
+			// アニメーション変更
+			this.player.anim.gotoAndPlay('fly');
+		}
+	},
+});
+
 // MainScene クラスを定義
 phina.define('MainScene', {
 	superClass: 'DisplayScene',
@@ -58,36 +157,31 @@ phina.define('MainScene', {
 		}).addChildTo(this).setPosition(this.gridX.center(), this.gridY.center(2));
 
 		// プレイヤー
-		var player = Player('tomapiko').addChildTo(this);
-		// 初期配置
-		player.x = this.gridX.center();
-		player.bottom = this.floor.top;
+		this.player = Player('tomapiko').addChildTo(this).setPosition(this.gridX.center(), this.floor.top);
 
 		// 画面タッチ時処理
 		this.onpointend = function(e) {
 			if (this.gameFlg) {
 				// シーンの中心を基準に力の加え方を変化させて向きを変化させる
 				if (Math.round(e.pointer.x) / (SCREEN_WIDTH / 2) > 1) {
-					player.physical.force(3, 0);
-					player.scaleX *= (player.scaleX == 1) ? -1 : 1;
+					this.player.physical.force(3, 0);
+					this.player.scaleX *= (this.player.scaleX == 1) ? -1 : 1;
 				} else {
-					player.physical.force(-3, 0);
-					player.scaleX *= (player.scaleX == 1) ? 1 : -1;
+					this.player.physical.force(-3, 0);
+					this.player.scaleX *= (this.player.scaleX == 1) ? 1 : -1;
 				}
 				// 上方向に速度を与える（ジャンプ）
-				player.physical.velocity.y = -JUMP_POWER;
+				this.player.physical.velocity.y = -JUMP_POWER;
 				// 重力復活
-				player.physical.gravity.y = GRAVITY;
+				this.player.physical.gravity.y = GRAVITY;
 				// 床上フラグ変更
-				player.isOnFloor = false;
+				this.player.isOnFloor = false;
 				// アニメーション変更
-				player.anim.gotoAndPlay('fly');
+				this.player.anim.gotoAndPlay('fly');
 				// 床フォードアウト
 				this.floor.tweener.clear().fadeOut(200);
 			}
 		}
-		// 参照用
-		this.player = player;
 	},
 
 	// ゲームオーバー処理
@@ -116,7 +210,6 @@ phina.define('MainScene', {
 			});
 	},
 
-
 	// 毎フレーム処理
 	update: function(app) {
 		var player = this.player;
@@ -131,12 +224,12 @@ phina.define('MainScene', {
 			balloon.point();
 		}
 
-		// 100フレーム毎にバルーンを生成
-		if ((app.ticker.frame % 100) == 0) {
+		// 100フレーム毎にゴーストを生成
+		if ((app.ticker.frame % 200) == 0) {
 			// ゴースト
 			var ghost = Ghost('ghost').addChildTo(this.ghostGroup);
 			// 初期配置
-			pointX = (app.ticker.frame % 200) ? 0 : SCREEN_WIDTH;
+			pointX = (app.ticker.frame % 400) ? 0 : SCREEN_WIDTH;
 			ghost.point(pointX);
 		}
 
@@ -194,6 +287,30 @@ phina.define('MainScene', {
 	}
 });
 
+// スタートラベル
+phina.define('Start', {
+	// 親クラス指定
+	superClass: 'Label',
+	// コンストラクタ
+	init: function() {
+		// 親クラス初期化
+		this.superInit({
+			text:'画面をタッチしてスタート',
+			fontSize: 42,
+			fill: 'white',
+		});
+	},
+
+	changeSize: function() {
+		this.tweener.clear()
+			.scaleTo(1.1, 300)
+			.scaleTo(0.9, 300)
+			.call(function() {
+				this.changeSize();
+			}, this);
+	},
+});
+
 // プレイヤー
 phina.define('Player', {
 	// 親クラス指定
@@ -206,8 +323,6 @@ phina.define('Player', {
 		this.anim = FrameAnimation('tomapiko_ss').attachTo(this);
 		// 初期アニメーション
 		this.anim.gotoAndPlay('stand');
-		// 初速度を与える
-//		this.physical.force(-3, 0);
 		// 床上フラグ
 		this.isOnFloor = true;
 	},
@@ -265,7 +380,6 @@ phina.define('Balloon', {
 	}
 });
 
-
 // ゴースト
 phina.define('Ghost', {
 	// 親クラス指定
@@ -301,7 +415,7 @@ phina.main(function() {
 	// アプリケーション生成
 	var app = GameApp({
 		startLabel: 'title',
-		title: 'Touch To Jump',
+		// title: 'Touch To Jump',
 		assets: ASSETS,
 		width: SCREEN_WIDTH,
 		height: SCREEN_HEIGHT,
